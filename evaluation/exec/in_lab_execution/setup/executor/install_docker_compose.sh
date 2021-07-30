4e7f60df9ab46b0ccb608b6ce4fb3e15d33f5f5a
Gabriel Rodrigues <gabrielsr@gmail.com>
2021-07-30 17:56:08 -0300

    executor setup

---
+++ evaluation/exec/in_lab_execution/setup/executor/install_docker_compose.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release git vim
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+git clone https://github.com/Gastd/morse_simulation.git
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo docker run hello-world
+cd morse_simulation/
+git submodule update --init --recursive
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo 'Reboot......'
